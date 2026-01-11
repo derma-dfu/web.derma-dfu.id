74460455ae940e1cc38be0aae7a0e6ffa40164d8
Ferri Krisdiantoro <ferri.krisdiantoro.study@gmail.com>
2026-01-11 09:51:33 +0700

    update

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -102,7 +102,7 @@ const Auth = () => {
             const { error } = await supabase.auth.signInWithOAuth({
                 provider: 'google',
                 options: {
-                    redirectTo: `${window.location.origin}/dashboard`,
+                    redirectTo: `${window.location.origin}/auth/callback`,
                 }
             });
 
